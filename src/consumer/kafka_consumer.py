e34fbaed34c3ec84ee98865f6b408dbbba0b6178
Ivan Mihun <vsk76090@gmail.com>
2025-06-27 20:41:40 +0200

    feat: implement KafkaConsumer with listen method for consuming and deserializing messages

---
+++ src/consumer/kafka_consumer.py
@@ -0,0 +1,25 @@
+from typing import AsyncIterator
+
+from aiokafka import AIOKafkaConsumer
+
+from src.consumer import Consumer
+from src.deserializer.deserializer import Deserializer
+from src.schemas.event import Event
+
+
+class KafkaConsumer(Consumer):
+    def __init__(self, consumer: AIOKafkaConsumer, deserializer: Deserializer):
+        self.consumer = consumer
+        self.deserializer = deserializer
+
+    async def listen(self) -> AsyncIterator[Event]:
+        await self.consumer.start()
+        try:
+            async for msg in self.consumer:
+                deserialized_payload = self.deserializer.deserialize(msg.value)
+
+                yield Event(topic=msg.topic,
+                            type=deserialized_payload.type,
+                            body=deserialized_payload.body)
+        finally:
+            await self.consumer.stop()
