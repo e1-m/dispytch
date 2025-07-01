682035942a7b2bbb134afc6d4652010dcbee304c
Ivan Mihun <vsk76090@gmail.com>
2025-07-01 11:36:43 +0200

    ♻️ refactor(di): remove obsolete dfs dependency resolution implementation
    - delete `solve_dependencies_dfs` function as it's no longer needed

--- src/di/solv/solver.py
+++ src/di/solv/solver.py
@@ -1,8 +1,7 @@
-from contextlib import asynccontextmanager, AsyncExitStack
+from contextlib import asynccontextmanager
 from typing import Callable, Any
 
 from src.di.models import EventHandlerContext
-from src.di.solv.alt import solve_dependencies_dfs
 from src.di.solv.builder import get_dependency_tree
 
 
@@ -11,9 +10,3 @@ async def solve_dependencies(func: Callable[..., Any], ctx: EventHandlerContext 
     tree = get_dependency_tree(func)
     async with tree.resolve(ctx) as deps:
         yield deps
-
-
-@asynccontextmanager
-async def solve_dependencies_alternative(func: Callable[..., Any], ctx: EventHandlerContext = None):
-    async with AsyncExitStack() as stack:  # noqa
-        yield await solve_dependencies_dfs(func, stack, {}, set(), ctx=ctx)
