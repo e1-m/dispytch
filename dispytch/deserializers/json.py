1b2dcc2eff219236f1904a56b84c1502f6b5edaf
Ivan Mihun <vsk76090@gmail.com>
2025-07-02 13:21:48 +0200

    ✨ feat(deserializers): add configurable encoding support to JSONDeserializer
    - introduce an encoding parameter with a default value of 'utf-8'

--- dispytch/deserializers/json.py
+++ dispytch/deserializers/json.py
@@ -5,8 +5,11 @@ from dispytch.deserializers.exc import FieldMissingError
 
 
 class JSONDeserializer(Deserializer):
+    def __init__(self, encoding='utf-8'):
+        self.encoding = encoding
+
     def deserialize(self, payload: bytes) -> Payload:
-        data = json.loads(payload.decode('utf-8'))
+        data = json.loads(payload.decode(self.encoding))
 
         required_fields = ['type', 'body']
         missing = [field for field in required_fields if data.get(field) is None]
