48f5d9c8616c819f030a5b8d00b42d9ed12e4e72
Ivan Mihun <vsk76090@gmail.com>
2025-07-03 14:10:12 +0200

    ✨ feat(consumers): enhance kafka consumer acknowledgment handling
    - add logging for non-existent or already acknowledged events in `ack`
    - implement retry mechanism with backoff for committing offsets

--- dispytch/consumers/kafka.py
+++ dispytch/consumers/kafka.py
@@ -1,10 +1,15 @@
+import asyncio
 from typing import AsyncIterator
+import logging
 
 from aiokafka import AIOKafkaConsumer, ConsumerRecord, TopicPartition
+from aiokafka.errors import KafkaError
 
 from dispytch.deserializers import JSONDeserializer
 from dispytch.listener.consumer import Consumer, Event
 from dispytch.consumers.deserializer import Deserializer
+
+logger = logging.getLogger(__name__)
 
 
 class KafkaConsumer(Consumer):
@@ -32,6 +37,27 @@ class KafkaConsumer(Consumer):
             yield event
 
     async def ack(self, event: Event):
-        msg = self._waiting_for_commit.pop(event.id)
+        try:
+            msg = self._waiting_for_commit.pop(event.id)
+        except KeyError as e:
+            logger.warning(f"Tried to ack a non-existent or already acked event {event.id}")
+            raise e
+
         tp = TopicPartition(msg.topic, msg.partition)
-        await self.consumer.commit({tp: msg.offset + 1})
+
+        max_retries = 3
+        backoff = 1
+
+        for attempt in range(1, max_retries + 1):
+            try:
+                return await self.consumer.commit({tp: msg.offset + 1})
+            except KafkaError as e:
+                if not e.retriable:
+                    raise e
+
+                if attempt == max_retries:
+                    logger.critical(f"Commit failed after {max_retries} attempts for event {event.id}")
+                    raise e
+
+                await asyncio.sleep(backoff * attempt)
+        return None
