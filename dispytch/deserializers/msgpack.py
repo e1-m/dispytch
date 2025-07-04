7fe14bac9781aa5cc2893b6c7662377629daebb0
Ivan Mihun <vsk76090@gmail.com>
2025-07-04 14:31:58 +0200

    ✨ feat(deserializer): add field validation to msgpack deserializer
    - validate required fields `type` and `body` during deserialization
    - raise `FieldMissingError` for missing fields

--- dispytch/deserializers/msgpack.py
+++ dispytch/deserializers/msgpack.py
@@ -1,8 +1,15 @@
 import msgpack
 
 from dispytch.consumers.deserializer import Deserializer, Payload
+from dispytch.deserializers.exc import FieldMissingError
 
 
 class MessagePackDeserializer(Deserializer):
     def deserialize(self, payload: bytes) -> Payload:
-        return Payload(**msgpack.unpackb(payload, raw=False))
+        data = msgpack.unpackb(payload, raw=False)
+
+        required_fields = ['type', 'body']
+        missing = [field for field in required_fields if data.get(field) is None]
+        if missing:
+            raise FieldMissingError(*missing)
+        return Payload(**data)
