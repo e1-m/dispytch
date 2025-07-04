092c891346ac389df009e7c46edb3e136d7153f2
Ivan Mihun <vsk76090@gmail.com>
2025-07-04 20:31:53 +0200

    ♻️ refactor(kafka): remove unused start and stop methods from Kafka producer

--- dispytch/producers/kafka.py
+++ dispytch/producers/kafka.py
@@ -10,11 +10,5 @@ class KafkaProducer(Producer):
         self.producer = producer
         self.serializer = serializer or JSONSerializer()
 
-    async def start(self) -> None:
-        await self.producer.start()
-
-    async def stop(self) -> None:
-        await self.producer.stop()
-
     async def send(self, topic: str, payload: dict) -> None:
         await self.producer.send(topic, value=self.serializer.serialize(payload))
