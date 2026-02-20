e8516a4ee3beab8951da5aa78f708434fda3812a
Ivan Mihun <vsk76090@gmail.com>
2026-02-20 12:18:20 +0100

    ✨ feat(middleware): add AsyncLock middleware for concurrency control
    - introduce AsyncLock to limit concurrency using asyncio semaphores
    - add key-based semaphore management for efficient locking mechanism
    - update __init__.py to include AsyncLock in middleware exports

--- dispytch/middleware/__init__.py
+++ dispytch/middleware/__init__.py
@@ -1,9 +1,11 @@
 from .retry import Retry
 from .filter import Filter
 from .exc_interceptor import ExceptionInterceptor
+from .async_lock import AsyncLock
 
 __all__ = [
     "Retry",
     "Filter",
     "ExceptionInterceptor",
+    "AsyncLock"
 ]
---
+++ dispytch/middleware/async_lock.py
@@ -0,0 +1,40 @@
+import asyncio
+from dataclasses import dataclass
+from typing import Callable, Hashable
+
+from dispytch import Middleware, EventHandlerContext, NextCall
+
+
+@dataclass
+class _SemaphoreEntry:
+    semaphore: asyncio.Semaphore
+    ref_count: int
+
+
+class AsyncLock(Middleware):
+    def __init__(
+            self,
+            key_extractor: Callable[[EventHandlerContext], Hashable],
+            concurrency_limit: int = 1
+    ):
+        self.key_extractor = key_extractor
+        self.concurrency_limit = concurrency_limit
+
+        self._semaphores: dict[Hashable, _SemaphoreEntry] = {}
+
+    async def dispatch(self, ctx: EventHandlerContext, call_next: NextCall):
+        key = self.key_extractor(ctx)
+
+        if key not in self._semaphores:
+            self._semaphores[key] = _SemaphoreEntry(semaphore=asyncio.Semaphore(self.concurrency_limit), ref_count=0)
+
+        self._semaphores[key].ref_count += 1
+
+        try:
+            async with self._semaphores[key].semaphore:
+                return await call_next(ctx)
+        finally:
+            self._semaphores[key].ref_count -= 1
+
+            if self._semaphores[key].ref_count == 0:
+                del self._semaphores[key]
