ea0b7f71089fad6dfb7e66eca2627aed0f46a309
Ivan Mihun <vsk76090@gmail.com>
2026-02-21 14:11:38 +0100

    ✨ feat(kafka): add fetch loop and in-flight message tracking to KafkaConsumer
    - introduce fetch loop for polling messages with configurable fetch interval
    - implement in-flight message count tracking per partition for flow control
    - add partition queue management for fetched records
    - enhance partition handling with cleanup logic for assigned/revoked partitions

--- dispytch/kafka/consumer.py
+++ dispytch/kafka/consumer.py
@@ -1,3 +1,4 @@
+import asyncio
 from dataclasses import dataclass
 from typing import AsyncIterator
 import logging
@@ -23,8 +24,15 @@ class _MessageCommitInfo:
 
 
 class KafkaConsumer(Consumer, ConsumerRebalanceListener):
-    def __init__(self, consumer: AIOKafkaConsumer, batch_timeout_ms: int = 1000, batch_size: int = 10):
+    def __init__(self,
+                 consumer: AIOKafkaConsumer,
+                 in_flight_msg_limit_per_partition: int = 100,
+                 fetch_interval_ms: int = 100,
+                 batch_timeout_ms: int = 1000,
+                 batch_size: int = 10):
         self.consumer = consumer
+        self.in_flight_msg_limit_per_partition = in_flight_msg_limit_per_partition
+        self.fetch_interval_ms = fetch_interval_ms
         self._batch_processor = BatchProcessor(
             handler=self._batch_commit,
             batch_timeout_ms=batch_timeout_ms,
@@ -32,12 +40,15 @@ class KafkaConsumer(Consumer, ConsumerRebalanceListener):
         )
         self._waiting_for_commit: dict[UUID, _MessageCommitInfo] = {}
         self._offset_tracker: dict[TopicPartition, OffsetTracker] = {}
+        self._in_flight_count: dict[TopicPartition, int] = {}
+        self._queues: dict[TopicPartition, asyncio.Queue] = {}
+        self._running = False
 
     async def start(self):
         existing_topics = self.consumer.subscription()
         if not existing_topics:
             raise RuntimeError("Consumer must be subscribed to topics before listening.")
-        if self.consumer._enable_auto_commit is True:
+        if self.consumer._enable_auto_commit:  # noqa
             raise RuntimeError("Consumer must have auto commit disabled before listening.")
 
         self.consumer.subscribe(topics=list(existing_topics), listener=self)
@@ -67,18 +78,52 @@ class KafkaConsumer(Consumer, ConsumerRebalanceListener):
         offset_tracker = self._offset_tracker.get(commit_info.tp, None)
         if offset_tracker is None:
             return
+        # Same
+        if commit_info.tp not in self._in_flight_count:
+            return
+
+        self._in_flight_count[commit_info.tp] -= 1
 
         offset_to_commit = offset_tracker.mark_processed(commit_info.offset)
         if offset_to_commit is not None:
             await self._batch_processor.add((commit_info.tp, offset_to_commit + 1))
 
-    async def _batch_commit(self, batch: list[tuple[TopicPartition, int]]):
-        await self.consumer.commit({tp: offset for tp, offset in batch})
-
     async def on_partitions_revoked(self, revoked: list[TopicPartition]):
         for tp in revoked:
             if tp in self._offset_tracker:
                 self._offset_tracker.pop(tp)
+            if tp in self._in_flight_count:
+                self._in_flight_count.pop(tp)
+            if tp in self._queues:
+                self._queues.pop(tp)
 
     async def on_partitions_assigned(self, assigned: list[TopicPartition]):
         ...
+
+    async def _batch_commit(self, batch: list[tuple[TopicPartition, int]]):
+        await self.consumer.commit({tp: offset for tp, offset in batch})
+
+    async def _fetch_loop(self):
+        while self._running:
+            for tp in self.consumer.assignment():
+                # Initialize partition in flight count if tp is seen for the first time
+                if tp not in self._in_flight_count:
+                    self._in_flight_count[tp] = 0
+
+                max_records_to_fetch = self.in_flight_msg_limit_per_partition - self._in_flight_count[tp]
+                # Skip if the limit for the partition is reached
+                if max_records_to_fetch <= 0:
+                    continue
+
+                batches = await self.consumer.getmany(tp, timeout_ms=0, max_records=max_records_to_fetch)
+                records = batches.get(tp, [])
+
+                for record in records:
+                    # Initialize the partition queue if tp is seen for the first time
+                    if tp not in self._queues:
+                        self._queues[tp] = asyncio.Queue()
+
+                    self._queues[tp].put_nowait(record)
+                    self._in_flight_count[tp] += 1
+
+            await asyncio.sleep(self.fetch_interval_ms / 1000.0)
