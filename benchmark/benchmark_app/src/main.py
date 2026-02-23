63534cefaa1050aaca3c9cb8f4de47aef77cb6ea
Ivan Mihun <vsk76090@gmail.com>
2026-02-23 12:17:18 +0100

    ✨ feat(benchmark): add configurable scenarios and resource pool simulation
    - introduce `config` module for dynamic scenario configuration and reusable settings
    - implement resource pool simulation with `get_resource` function and semaphore-based control
    - enhance io simulation with configurable processing time range

--- benchmark/benchmark_app/src/main.py
+++ benchmark/benchmark_app/src/main.py
@@ -6,32 +6,40 @@ from prometheus_client import start_http_server
 
 from dispytch import EventDispatcher
 from dispytch.kafka import KafkaConsumer
-from dispytch.serialization.orjson import ORJSONDeserializer
+from dispytch.serialization.json import JSONDeserializer
 
-from .raw_throughput import router
 from .middleware import EventsInProgressMiddleware, LatencyMiddleware, TotalCounterMiddleware
+from .config import config
+
+if config.SCENARIO == "raw":
+    from .raw_throughput import router
+elif config.SCENARIO == "with-io":
+    from .io_simulation import router
+elif config.SCENARIO == "with-resource-pool":
+    from .resource_pool_simulation import router
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
 async def main():
-    kafka_consumer = AIOKafkaConsumer('benchmark_events',
-                                      bootstrap_servers='kafka:9092',
-                                      enable_auto_commit=False,
-                                      group_id='test_group',
-                                      fetch_max_bytes=1024 * 1000 * 10,
-                                      max_partition_fetch_bytes=1024 * 1000,
-                                      )
+    kafka_consumer = AIOKafkaConsumer(
+        'benchmark_events',
+        bootstrap_servers='kafka:9092',
+        enable_auto_commit=False,
+        group_id='test_group',
+        fetch_max_bytes=(config.MESSAGE_SIZE_BYTES * 2) * config.IN_FLIGHT_MSG_LIMIT_PER_PARTITION * 10,
+        max_partition_fetch_bytes=(config.MESSAGE_SIZE_BYTES * 2) * config.IN_FLIGHT_MSG_LIMIT_PER_PARTITION,
+    )
 
     consumer = KafkaConsumer(kafka_consumer,
-                             batch_timeout_ms=5000,
-                             fetch_interval_ms=50,
-                             batch_size=500,
-                             in_flight_msg_limit_per_partition=500)
+                             batch_timeout_ms=config.BATCH_COMMIT_TIMEOUT_MS,
+                             batch_size=config.BATCH_COMMIT_BATCH_SIZE,
+                             fetch_interval_ms=config.FETCH_INTERVAL_MS,
+                             in_flight_msg_limit_per_partition=config.IN_FLIGHT_MSG_LIMIT_PER_PARTITION)
 
     event_listener = EventDispatcher(consumer,
-                                     deserializer=ORJSONDeserializer(),
+                                     deserializer=JSONDeserializer(),
                                      middlewares=[EventsInProgressMiddleware(),
                                                   LatencyMiddleware(),
                                                   TotalCounterMiddleware()])
--- benchmark/benchmark_app/src/resource_pool_simulation.py
+++ benchmark/benchmark_app/src/resource_pool_simulation.py
@@ -1,11 +1,13 @@
 import asyncio
 import logging
 import random
+from typing import Annotated
 
 from pydantic import BaseModel
 
-from dispytch import Router, Event
+from dispytch import Router, Event, Dependency
 from dispytch.kafka import KafkaEventSubscription
+
 from .config import config
 
 logger = logging.getLogger(__name__)
@@ -18,8 +20,16 @@ class BenchmarkEvent(BaseModel):
     payload: str
 
 
+semaphore = asyncio.Semaphore(config.MAX_RESOURCE_POOL_SIZE)
+
+
+async def get_resource():
+    async with semaphore:
+        yield "resource"
+
+
 @router.handler(KafkaEventSubscription(topic='benchmark_events'))
-async def handle_event(event: Event[BenchmarkEvent]):
+async def handle_event(event: Event[BenchmarkEvent], resource: Annotated[str, Dependency(get_resource)]):
     await asyncio.sleep(
         random.uniform(
             config.MIN_PROCESSING_TIME_MS / 1000,
