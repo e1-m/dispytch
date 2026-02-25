b9328b54a5505a1f2fe69d42b5fa3852760c7018
Ivan Mihun <vsk76090@gmail.com>
2026-02-25 18:04:02 +0100

    :bug: refactor: add retry policy to __init__ reimports

--- dispytch/middleware/__init__.py
+++ dispytch/middleware/__init__.py
@@ -1,10 +1,14 @@
 from .retry import Retry
+from .retry import RetryPolicy
+from .retry import ExponentialBackoffWithFullJitter
 from .filter import Filter
 from .exc_interceptor import ExceptionInterceptor
 from .async_lock import AsyncLock
 
 __all__ = [
     "Retry",
+    "RetryPolicy",
+    "ExponentialBackoffWithFullJitter",
     "Filter",
     "ExceptionInterceptor",
     "AsyncLock"
